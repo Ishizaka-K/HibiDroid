b340897e6d74cba5f330e208a0f882b7572a0fb4
Ishizaka-K <kento@i3-systems.com>
2024-04-15 14:18:09 +0900

    first commit

---
+++ app/src/main/java/com/example/hibidroid/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.hibidroid
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.hibidroid.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
