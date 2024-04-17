477f39cfb6518ae45cf229cecd62df0cec222f43
Ishizaka-K <kento@i3-systems.com>
2024-04-17 20:28:17 +0900

    Kintai

--- app/src/main/java/com/example/hibidroid/Connector/KintaiConnector.kt
+++ app/src/main/java/com/example/hibidroid/Connector/KintaiConnector.kt
@@ -14,6 +14,7 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import org.json.JSONArray
 import org.json.JSONException
+import org.json.JSONObject
 import java.io.IOException
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
@@ -21,174 +22,181 @@ import java.util.concurrent.TimeUnit
 
 class KintaiConnector {
     //接続先のurl
-    private var urlStr = ""
+//    private var urlStr = ""
+//
+//    //レスポンス用文字列
+//    private var respStr = ""
+//
+//    //レスポンス用JSONArray
+//    private var respJsonArr: JSONArray? = null
+//
+//    //待機処理用
+//    private var Roop_Count = 0
+//    private var Timeout_Sec = 0
+//    private var countDownLatch: CountDownLatch? = null
+//
+//    //setterとgetter
+//    fun getUrl(): String {
+//        return urlStr
+//    }
+//
+//    fun setUrl(urlStr: String) {
+//        this.urlStr = urlStr
+//    }
+//
+//    //setterとgetter(レスポンス用　非同期処理対応)
+//    @Throws(InterruptedException::class)
+//    fun getrespStr(): String {
+//        countDownLatch!!.await(Timeout_Sec.toLong(), TimeUnit.SECONDS)
+//        return respStr
+//    }
+//
+//    fun setrespStr(respStr: String) {
+//        countDownLatch!!.countDown()
+//        this.respStr = respStr
+//    }
+//
+//    @Throws(InterruptedException::class)
+//    fun getrespJsonArr(): JSONArray? {
+//        countDownLatch!!.await(Timeout_Sec.toLong(), TimeUnit.SECONDS)
+//        return respJsonArr
+//    }
+//
+//    fun setrespJsonArr(respJsonArr: JSONArray?) {
+//        countDownLatch!!.countDown()
+//        this.respJsonArr = respJsonArr
+//    }
+//
+//    //コンストラクタ
+//    fun HttpConnector(Roop_Count: Int, Timeout_Sec: Int) {
+//        this.Roop_Count = Roop_Count
+//        this.Timeout_Sec = Timeout_Sec
+//        countDownLatch = CountDownLatch(Roop_Count)
+//    }
+//
+////    //端末がネットワークに接続されているか確認
+////    fun checkConnection(context: Context): Boolean {
+////        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+////        val info = cm.getNetworkCapabilities(cm.activeNetwork)
+////        return info != null
+////    }
+//
+//    //GET実行
+//    fun doGet(urlStr: String?) {
+//        val request: Request = Request.Builder()
+//            .url(urlStr!!)
+//            .get()
+//            .build()
+//        conectHttp(request)
+//    }
+//
+//    //POST実行
+//    fun doPost(url: String?, postdata: String?) {
+//        val body: RequestBody =
+//            RequestBody.create(postdata, "application/json; charset=Shift_JIS".toMediaType())
+//        val request: Request = Request.Builder()
+//            .url(url!!)
+//            .post(body)
+//            .build()
+//        conectHttp(request)
+//    }
+//
+//    //Requestを送りResponseを受け取る
+//    private fun conectHttp(request: Request) {
+//        val client = OkHttpClient()
+//        client.newCall(request).enqueue(object : Callback() {
+//            //通信に失敗した場合
+//            fun onFailure(call: Call?, e: IOException) {
+//                e.printStackTrace()
+//                setrespStr("")
+//                setrespJsonArr(null)
+//                Log.d("状態", "Failure")
+//            }
+//
+//            //通信に成功した場合
+//            fun onResponse(call: Call?, response: Response) {
+//                try {
+//                    setrespStr(response.body().string())
+//                } catch (e: IOException) {
+//                    e.printStackTrace()
+//                    Log.d("状態", "Empty")
+//                    setrespStr("")
+//                } catch (e: NullPointerException) {
+//                    e.printStackTrace()
+//                    Log.d("状態", "Empty")
+//                    setrespStr("")
+//                }
+//                try {
+//                    setrespJsonArr(JSONArray(respStr))
+//                } catch (e: JSONException) {
+//                    e.printStackTrace()
+//                    Log.d("状態", "Empty")
+//                    setrespJsonArr(null)
+//                }
+//            }
+//        })
+//    }
 
-    //レスポンス用文字列
-    private var respStr = ""
 
-    //レスポンス用JSONArray
-    private var respJsonArr: JSONArray? = null
+    fun startGetRequest(client: OkHttpClient,request: Request): String {
 
-    //待機処理用
-    private var Roop_Count = 0
-    private var Timeout_Sec = 0
-    private var countDownLatch: CountDownLatch? = null
+        val JSON_MEDIA = "application/json; charset=utf-8".toMediaType()
 
-    //setterとgetter
-    fun getUrl(): String {
-        return urlStr
-    }
+//        var client: OkHttpClient = OkHttpClient().newBuilder()
+//            .readTimeout((15 * 1000).toLong(), TimeUnit.MILLISECONDS)
+//            .writeTimeout((20 * 1000).toLong(), TimeUnit.MILLISECONDS)
+//            .connectTimeout((20 * 1000).toLong(), TimeUnit.MILLISECONDS)
+//            .build()
+//
+//        // Requestを作成
+//        val request = Request.Builder()
+//            .url("https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&current_weather=true")
+//            .build()
 
-    fun setUrl(urlStr: String) {
-        this.urlStr = urlStr
-    }
-
-    //setterとgetter(レスポンス用　非同期処理対応)
-    @Throws(InterruptedException::class)
-    fun getrespStr(): String {
-        countDownLatch!!.await(Timeout_Sec.toLong(), TimeUnit.SECONDS)
-        return respStr
-    }
-
-    fun setrespStr(respStr: String) {
-        countDownLatch!!.countDown()
-        this.respStr = respStr
-    }
-
-    @Throws(InterruptedException::class)
-    fun getrespJsonArr(): JSONArray? {
-        countDownLatch!!.await(Timeout_Sec.toLong(), TimeUnit.SECONDS)
-        return respJsonArr
-    }
-
-    fun setrespJsonArr(respJsonArr: JSONArray?) {
-        countDownLatch!!.countDown()
-        this.respJsonArr = respJsonArr
-    }
-
-    //コンストラクタ
-    fun HttpConnector(Roop_Count: Int, Timeout_Sec: Int) {
-        this.Roop_Count = Roop_Count
-        this.Timeout_Sec = Timeout_Sec
-        countDownLatch = CountDownLatch(Roop_Count)
-    }
-
-    //端末がネットワークに接続されているか確認
-    fun checkConnection(context: Context): Boolean {
-        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val info = cm.getNetworkCapabilities(cm.activeNetwork)
-        return info != null
-    }
-
-    //GET実行
-    fun doGet(urlStr: String?) {
-        val request: Request = Request.Builder()
-            .url(urlStr!!)
-            .get()
-            .build()
-        conectHttp(request)
-    }
-
-    //POST実行
-    fun doPost(url: String?, postdata: String?) {
-        val body: RequestBody =
-            RequestBody.create(postdata, "application/json; charset=Shift_JIS".toMediaType())
-        val request: Request = Request.Builder()
-            .url(url!!)
-            .post(body)
-            .build()
-        conectHttp(request)
-    }
-
-    //Requestを送りResponseを受け取る
-    private fun conectHttp(request: Request) {
-        val client = OkHttpClient()
-        client.newCall(request).enqueue(object : Callback() {
-            //通信に失敗した場合
-            fun onFailure(call: Call?, e: IOException) {
-                e.printStackTrace()
-                setrespStr("")
-                setrespJsonArr(null)
-                Log.d("状態", "Failure")
-            }
-
-            //通信に成功した場合
-            fun onResponse(call: Call?, response: Response) {
-                try {
-                    setrespStr(response.body().string())
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                    Log.d("状態", "Empty")
-                    setrespStr("")
-                } catch (e: NullPointerException) {
-                    e.printStackTrace()
-                    Log.d("状態", "Empty")
-                    setrespStr("")
-                }
-                try {
-                    setrespJsonArr(JSONArray(respStr))
-                } catch (e: JSONException) {
-                    e.printStackTrace()
-                    Log.d("状態", "Empty")
-                    setrespJsonArr(null)
-                }
-            }
-        })
-    }
-
-    private val client = OkHttpClient.Builder()
-        .connectTimeout(CONNECTION_TIMEOUT_MILLISECONDS.toLong(), TimeUnit.MILLISECONDS)
-        .readTimeout(READ_TIMEOUT_MILLISECONDS.toLong(), TimeUnit.MILLISECONDS)
-        .build()
-
-    override fun startGetRequest() {
-        // Requestを作成
-        val request = Request.Builder()
-            .url("ここにURL")
-            .build()
-
+        var responseBody: String = String()
+        var resposeJson: Response
         client.newCall(request).enqueue(object : Callback {
-            override fun onResponse(call: Call, response: Response) {
-                // Responseの読み出し
-                val responseBody = response.body?.string().orEmpty()
-                // 必要に応じてCallback
+            override fun onResponse(call: okhttp3.Call, response: Response) {
+                resposeJson = response
+                Log.d("Response", response.body.toString())
+                responseBody = response.headers.toString()
             }
 
-            override fun onFailure(call: Call, e: IOException) {
+            override fun onFailure(call: okhttp3.Call, e: IOException) {
                 Log.e("Error", e.toString())
                 // 必要に応じてCallback
             }
         })
+        return responseBody
     }
 
+    fun startPostRequest() {
 
+        val JSON_MEDIA = "application/json; charset=utf-8".toMediaType()
 
-    //POST
-    private val client = OkHttpClient.Builder()
-        .connectTimeout(CONNECTION_TIMEOUT_MILLISECONDS.toLong(), TimeUnit.MILLISECONDS)
-        .readTimeout(READ_TIMEOUT_MILLISECONDS.toLong(), TimeUnit.MILLISECONDS)
-        .build()
+        var client: OkHttpClient = OkHttpClient().newBuilder()
+            .readTimeout((15 * 1000).toLong(), TimeUnit.MILLISECONDS)
+            .writeTimeout((20 * 1000).toLong(), TimeUnit.MILLISECONDS)
+            .connectTimeout((20 * 1000).toLong(), TimeUnit.MILLISECONDS)
+            .build()
 
-    private val JSON_MEDIA = "application/json; charset=utf-8".toMediaType()
-
-    override fun startPostRequest() {
         // Bodyのデータ（サンプル）
         val sendDataJson = "{\"id\":\"1234567890\",\"name\":\"hogehoge\"}"
 
         // Requestを作成
         val request = Request.Builder()
-            .url(urlStr)
+            .url("inflater")
             .post(sendDataJson.toRequestBody(JSON_MEDIA))
             .build()
 
         client.newCall(request).enqueue(object : Callback {
-            override fun onResponse(call: Call, response: Response) {
+            override fun onResponse(call: okhttp3.Call, response: Response) {
                 // Responseの読み出し
-                val responseBody = response.body?.string().orEmpty()
+                val responseBody = response.headers.toString().orEmpty()
                 // 必要に応じてCallback
             }
 
-            override fun onFailure(call: Call, e: IOException) {
+            override fun onFailure(call: okhttp3.Call, e: IOException) {
                 Log.e("Error", e.toString())
                 // 必要に応じてCallback
             }
--- app/src/main/java/com/example/hibidroid/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/hibidroid/ui/main/MainFragment.kt
@@ -2,12 +2,20 @@ package com.example.hibidroid.ui.main
 
 import androidx.fragment.app.viewModels
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
+import com.example.hibidroid.Connector.KintaiConnector
 
 import com.example.hibidroid.R
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.util.concurrent.TimeUnit
 
 class MainFragment : Fragment() {
 
@@ -25,9 +33,29 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        savedInstanceState: Bundle?): View {
+
+        var mRootView = inflater.inflate(R.layout.fragment_main, container, false)
+
+        mRootView.findViewById<Button>(R.id.button).setOnClickListener {
+            var msg = requireView().findViewById<TextView>(R.id.message)
+
+            var client: OkHttpClient = OkHttpClient().newBuilder()
+                .readTimeout((15 * 1000).toLong(), TimeUnit.MILLISECONDS)
+                .writeTimeout((20 * 1000).toLong(), TimeUnit.MILLISECONDS)
+                .connectTimeout((20 * 1000).toLong(), TimeUnit.MILLISECONDS)
+                .build()
+
+            val request = Request.Builder()
+                .url("https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&current_weather=true")
+                .build()
+            var a = KintaiConnector().startGetRequest(client,request)
+            msg.text = a
+            Log.d("aaa",a.toString())
+        }
+
+        return mRootView
     }
+
 
 }
